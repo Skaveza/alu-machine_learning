7654ae419667d122302229c85d14198b510596f5
Skaveza <s.mwahoni@alustudent.com>
2024-09-07 17:14:04 +0200

    5-all_in_one.py

--- math/plotting/5-all_in_one.py
+++ math/plotting/5-all_in_one.py
@@ -25,4 +25,44 @@ y32 = np.exp((r3 / t32) * x3)
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-plt.title("All in One")
+fig=plt.figure()
+
+ax1=fig.add_subplot(3,2,1)
+ax1.plot(y0,c="red")
+ax1.set_xlim(0,11)
+
+ax2=fig.add_subplot(3,2,2)
+ax2.set_xlabel("Height(in)", fontsize='x-small')
+ax2.set_ylabel("Weight(lbs)", fontsize='x-small')
+ax2.set_title ("Men\'s Height vs. Weight", fontsize='x-small')
+ax2.scatter(x1,y1, c="magenta")
+
+ax3=fig.add_subplot(3,2,3)
+ax3.set_xlabel("Time(years)",fontsize='x-small' )
+ax3.set_ylabel("Fraction Remaining", fontsize='x-small')
+ax3.set_title("Exponential Decay of C-14", fontsize='x-small')
+ax3.set_yscale('log')
+ax3.set_xlim(0,28650)
+ax3.plot(x2,y2, c="blue")
+
+ax4=fig.add_subplot(3,2,4)
+ax4.set_xlabel("Time(years)", fontsize='x-small')
+ax4.set_ylabel("Fraction Remaining", fontsize='x-small')
+ax4.set_title("Exponential Decay of Radioactive Elements", fontsize='x-small')
+ax4.set_xlim(0,20000)
+ax4.set_ylim(0,1)
+ax4.plot(x3,y31, linestyle="dashed", c="red", label="C-14")
+ax4.plot(x3,y32, c="green", label="Ra-226")
+
+ax4.legend()
+
+ax5=fig.add_subplot(3,1,3)
+ax5.set_xlabel("Grades", fontsize='x-small')
+ax5.set_ylabel("Number of Students",fontsize='x-small' )
+ax5.hist(student_grades, edgecolor="black", bins=np.arange(40,110,10))
+ax5.set_title("Project A", fontsize='x-small')
+
+fig.suptitle("All in One")
+plt.tight_layout()
+plt.show
+
