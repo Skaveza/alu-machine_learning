0f3ff7f2e0f6021ed6997e27d81d7d6e8b105a52
Skaveza <s.mwahoni@alustudent.com>
2024-09-12 06:01:38 +0200

    updates

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -11,16 +11,16 @@ print(matrix_shape(mat2))
 
 def matrix_shape(matrix):
   """ Calculates the shape of a matrix
-  
+
   Args:
      matrix: The matrix to calculate the shape of.
-     
+    
   Returns:
      A list containing the shape of the matrix in each dimension.
     """
-  
+
   shape = []
   while isinstance(matrix, list):
     shape.append(len(matrix))
     matrix = matrix[0]
-    return shape
+  return shape
