a8cbe11553fdb5eda7aa4707f6e638368b0e8c4b
Skaveza <s.mwahoni@alustudent.com>
2024-09-12 06:26:53 +0200

    update

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -13,14 +13,13 @@ print(matrix_transpose(mat2))
 
 
 def matrix_transpose(matrix):
-  """Transposes a matrix.
+    """Transposes a matrix.
+    
+    Args:
+        matrix: A 2D list (matrix) to transpose.
+    
+    Returns:
+        A new transposed matrix where the rows and columns are swapped.
+    """
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
   
-  Args:
-  matrix: A 2D list (matrix) to transpose.
-  
-  Returns:
-  A new transposed matrix where the rows and columns are swapped.
-  """
-
-  return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
- 
