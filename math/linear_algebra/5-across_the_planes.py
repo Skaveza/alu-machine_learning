a0e660574c2cf1994983a57a78db26141cf11004
Skaveza <s.mwahoni@alustudent.com>
2024-09-12 06:50:41 +0200

    update

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -29,4 +29,7 @@ def add_matrices2D(mat1, mat2):
         if len(row1) != len(row2):
             return None
 
-    return [[row1[i] + row2[i] for i in range(len(row1))] for row1, row2 in zip(mat1, mat2)]
+    return [
+        [row1[i] + row2[i] for i in range(len(row1))]
+        for row1, row2 in zip(mat1, mat2)
+    ]
