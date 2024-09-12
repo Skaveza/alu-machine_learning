a45b7638000f4e4e7092948ed7d542157d1c0edc
Skaveza <s.mwahoni@alustudent.com>
2024-09-12 08:07:40 +0200

    updates

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -10,6 +10,7 @@ mat2 = [[1, 2, 3, 4],
 print(mat_mul(mat1, mat2))
 """
 
+
 def mat_mul(mat1, mat2):
     """Performs matrix multiplication between two 2D matrices.
 
@@ -21,15 +22,15 @@ def mat_mul(mat1, mat2):
         A new 2D list that is the result of multiplying mat1 by mat2.
         If the matrices cannot be multiplied, returns None.
     """
-  
+
     if len(mat1[0]) != len(mat2):
         return None
 
     result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
 
-    for i in range(len(mat1)):  
-        for j in range(len(mat2[0])):  
-            for k in range(len(mat2)): 
+    for i in range(len(mat1)):
+        for j in range(len(mat2[0])):
+            for k in range(len(mat2)):
                 result[i][j] += mat1[i][k] * mat2[k][j]
 
     return result
