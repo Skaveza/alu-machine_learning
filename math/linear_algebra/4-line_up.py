aa7cf5a9213e3e211daf01f5f46c8255ef89fa18
Skaveza <s.mwahoni@alustudent.com>
2024-09-12 06:29:42 +0200

    update

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -12,16 +12,16 @@ print(add_arrays(arr1, [1, 2, 3]))
 
 def add_arrays(arr1, arr2):
   """Adds two arrays element-wise.
-  
+
   Args:
     arr1: A list of integers and floats.
     arr2: Alist of integers and floats.
-    
+  
   Returns:
   A new list with the elemnt-wise sum of ar1 aand arr2.
   If arr1 and arr2 are not the same shape, returns None.
   """
   if len(arr1) != len(arr2):
     return None
-  
+
   return [arr1[i] + arr2[i] for i in range(len(arr1))]
