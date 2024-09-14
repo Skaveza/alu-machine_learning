b9c01af83d7f8af8f3eb04a6bb1199ffce375d1b
Skaveza <s.mwahoni@alustudent.com>
2024-09-14 18:28:22 +0200

    task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,7 @@
 """This module concatenates two matrices along a specific axis."""
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
     """Concatenates two matrices along a specific axis.
 
