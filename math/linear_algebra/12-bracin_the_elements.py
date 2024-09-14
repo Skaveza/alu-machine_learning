3af8ad7bdb0154241b225d8980ea2d80008e7b6d
Skaveza <s.mwahoni@alustudent.com>
2024-09-14 18:12:06 +0200

    change

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 """
-Module that performs element-wise addition,subtraction, multiplication and division
+Module that performs element-wise operations.
 """
-
 
 def np_elementwise(mat1, mat2):
     """Performs element-wise addition, subtraction, multiplication, and division.
-    
+
     Args:
-        mat1: A numpy array.
-        mat2: A numpy array or scalar value.
+        mat1: A NumPy array.
+        mat2: A NumPy array or scalar.
 
     Returns:
-        A tuple containing the element-wise sum, difference, product, and quotient (in that order).
+        A tuple with element-wise sum, difference, product, and quotient.
     """
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
