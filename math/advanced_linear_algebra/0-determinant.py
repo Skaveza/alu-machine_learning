8367ae76ae9b1de1d9208e9901aee2751932ccda
Skaveza <s.mwahoni@alustudent.com>
2024-09-19 15:36:21 +0200

    update

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
 """This module calculates the determinant of a matrix"""
 
-
 def determinant(matrix):
     """Calculates the determinant of a matrix.
-
+  
     Args:
         matrix (list): A list whose determinant should be calculated.
-
+   
     Returns:
         int or float: The determinant of the matrix.
 
     Raises:
         TypeError: If matrix is not a list of lists.
         ValueError: If matrix is not a square matrix.
-    """
 
+    Example:
+        >>> determinant([[1, 2], [3, 4]])
+        -2
+  
+        >>> determinant([[5]])
+        5
+    """
 
     if not isinstance(matrix, list) or any(not isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -24,9 +29,9 @@ def determinant(matrix):
         return 1
 
     if any(
-    len(row) != len(matrix) for row in matrix
-):
-     raise ValueError("matrix must be a square matrix")
+        len(row) != len(matrix) for row in matrix
+    ):
+        raise ValueError("matrix must be a square matrix")
 
     if len(matrix) == 1:
         return matrix[0][0]
@@ -36,7 +41,9 @@ def determinant(matrix):
 
     det = 0
     for col in range(len(matrix)):
-        sub_matrix = [row[:col] + row[col+1:] for row in matrix[1:]]
+        sub_matrix = [
+            row[:col] + row[col+1:] for row in matrix[1:]
+        ]
         det += ((-1) ** col) * matrix[0][col] * determinant(sub_matrix)
 
     return det
