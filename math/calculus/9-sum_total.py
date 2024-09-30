b60a5ad49975a26f07bff90c2cbcd2d40d6f0d43
Skaveza <s.mwahoni@alustudent.com>
2024-09-30 17:17:08 +0200

    updates

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,7 @@
 def summation_i_squared(n):
     "Calculates the sum of squares from 1 to n"
     if type(n) is not int or n < 1:
-     return None
-  
+      return None
+
     sigma_sum = (n * (n + 1) * (2*n + 1)) // 6
     return int(sigma_sum)
