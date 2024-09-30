b10d74d5febec039246a2c0f0655dfe554d72602
Skaveza <s.mwahoni@alustudent.com>
2024-09-30 17:08:39 +0200

    update

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 "This module is used to calculate the sum of squares"
 
+
 def summation_i_squared(n):
     "Calculates the sum of squares from 1 to n"
     return (n * (n + 1) * (2*n + 1)) // 6
