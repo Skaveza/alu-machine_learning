660dc21ddf846f46879b11c3e9e4cc2af47220a0
Skaveza <s.mwahoni@alustudent.com>
2024-10-05 13:53:23 +0200

    update

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
-"This module calculates the derivative of a polynomial"
+'''
+    This function calculates the derivative of a polynomial
+'''
 
 
 def poly_derivative(poly):
-  "returns derivative of a polynomial"
-  if not isinstance(poly, list):
-    return None
+    '''
+    Returns the derivative of a polynomial
+    '''
+    if not isinstance(poly, list):
+        return None
 
-  if len(poly) == 1:
-    return [0]
+    if len(poly) == 1:
+        return [0]
 
-  derivative = []
-  for power, coef in enumerate(poly[1:], start=1):
-      derivative.append(coef * power)
+    derivative = []
+    for power, coef in enumerate(poly[1:], start=1):
+        derivative.append(coef * power)
 
-  return derivative if any (derivative) else [0]
-  
+    if not derivative:
+        return None
+
+    return derivative
