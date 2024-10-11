ed67184c1c42ff828e302a9852d866c134471c68
Skaveza <s.mwahoni@alustudent.com>
2024-10-11 21:13:09 +0200

    Formating changes

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -10,16 +10,20 @@ class Poisson:
         Initialize the Poisson distribution.
 
         Parameters:
-        data:A list of data points to estimate the distribution.
-        lambtha:The expected number of occurrences in a given time frame
-        If data is provided, lambtha is calculated as the mean of the data. 
-        Otherwise, the provided lambtha is used
+        data: A list of data points to estimate the distribution.
+        lambtha: The expected number of occurrences in a given time frame.
+
+        If data is provided, lambtha is calculated as the mean of the data.
+        Otherwise, the provided lambtha is used.
+
         Raises:
-        TypeError:If data is not a list.
-        ValueError:If lambtha is not positive or if data has fewer than two data points.
+        TypeError: If data is not a list.
+        ValueError: If lambtha is not positive or if data has fewer 
+        than two data points.
         """
         if lambtha <= 0:
             raise ValueError("lambtha must be a positive value")
+
         if data is None:
             self.lambtha = float(lambtha)
         else:
