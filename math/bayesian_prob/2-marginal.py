b57d43f1f883d38fcdbcdd0a2793f7e70d4da615
Skaveza <s.mwahoni@alustudent.com>
2024-10-18 18:24:09 +0200

    output check fixed

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -39,7 +39,9 @@ def intersection(x, n, P, Pr):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be >= 0")
+        raise ValueError(
+          "x must be an integer that is greater than or equal to 0"
+          )
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
@@ -79,7 +81,8 @@ def marginal(x, n, P, Pr):
     if not isinstance(n, int) or n <= 0:
         raise ValueError("n must be a positive integer")
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be >= 0")
+        raise ValueError(
+          "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
