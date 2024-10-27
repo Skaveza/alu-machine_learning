ce790bf2a7a3c27237cb78a126b534a9a640f68d
Skaveza <s.mwahoni@alustudent.com>
2024-10-27 15:29:06 +0200

    updated documentation

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 import numpy as np
-
-
 """
 Performs convolutions on grayscale images using the "valid" padding strategy.
 """
@@ -21,12 +19,14 @@ def convolve_grayscale_valid(images, kernel):
     """
     m, h, w = images.shape
     kh, kw = kernel.shape
-    output_h = h - kh + 1
-    output_w = w - kw + 1
+    output_h = h - kh + 1  # Valid padding: output height = input height - kernel height + 1
+    output_w = w - kw + 1  # Valid padding: output width = input width - kernel width + 1
 
-    convolved_images = np.zeros((m, output_h, output_w)) 
+    convolved_images = np.zeros((m, output_h, output_w))  # Pre-allocate output with correct shape
+
     for i in range(output_h):
         for j in range(output_w):
+            # Element-wise multiplication and summation within the valid region
             convolved_images[:, i, j] = np.sum(images[:, i:i + kh, j:j + kw] * kernel, axis=(1, 2))
 
     return convolved_images
