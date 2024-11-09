9e867456c7d8eb58a11c79b9efa2eaaac12c899a
Skaveza <s.mwahoni@alustudent.com>
2024-11-09 22:16:12 +0200

    indentation fixes

--- Summatives/Linear_Regression_Model_Summative_2/api.py
+++ Summatives/Linear_Regression_Model_Summative_2/api.py
@@ -3,23 +3,23 @@ from pydantic import BaseModel
 import joblib
 import numpy as np 
 
-model = joblib.load('heart_disease_model.joblib')
-app = FastAPI
 
+model = joblib.load('heart_disease_model.joblib')
+app = FastAPI()
 class PredictionRequest(BaseModel):
-  HighBP:int
-  BMI:float
-  Smoker:int
-  Sex:int
-  
-  #prediction route
-  app.post('/predict')
-  def predict (data:PredictionRequest):
+    HighBP: int
+    BMI: float
+    Smoker: int
+    Sex: int
+
+@app.post('/predict')
+def predict(data: PredictionRequest):
+    
     input_data = np.array([[data.HighBP, data.BMI, data.Smoker, data.Sex]])
-    #Getting prediction
+    # Get continuous prediction 
     y_pred_continuous = model.predict(input_data)[0]
     threshold = 0.5
     y_pred_binary = 1 if y_pred_continuous >= threshold else 0
     
+    
     return {'heart_disease_risk': y_pred_binary}
-  
