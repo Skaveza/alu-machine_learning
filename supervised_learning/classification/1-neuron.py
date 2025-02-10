877d42e663df373455b6d57535890ceb748d1e4d
Skaveza <s.mwahoni@alustudent.com>
2025-02-10 16:45:35 +0200

    task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,34 @@
+#!usr/bin/env/python3
+
+import numpy as np
+
+
+"""This module defines a single neuron performing binary classification"""
+
+class Neuron: """Defines the single neuron"""
+
+def __init__(self,nx):
+  """nx represents the number of input features to a neuron"""
+  
+  if isinstance(nx,int): raise TypeError
+  """nx must be an integer"""
+
+if nx < 1: raise ValueError 
+"""nx must be positive"""
+
+#Private attributes
+"""Initializing private instance attributes, each private attribute has a corresponding getter function"""
+self .__W = np.random.randn(1,nx)
+self .__b = 0
+self .__A = 0 
+
+#Getters
+"""These are the getters for the private attributes"""
+def W(self):
+        return self.__W
+      
+def b(self):
+        return self.__b
+      
+def A(self):
+        return self.__A
