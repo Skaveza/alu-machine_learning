0581088e8d393de0e2dc6ed695bb96d9c64f9ab7
Skaveza <s.mwahoni@alustudent.com>
2025-03-05 04:02:41 +0200

    added documenting

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python 3
+#!/usr/bin/env python3
 
 import requests
+
+"""This module fetches the list of starships that can hold at least 'passengerCount' passengers."""
+
 
 def availableShips(passengerCount):
     """
