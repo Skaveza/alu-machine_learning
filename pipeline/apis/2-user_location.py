e1e2af78263d70d72e540ab8dc5a9ca5e210831a
Skaveza <s.mwahoni@alustudent.com>
2025-03-11 12:32:08 +0200

    updated task 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-'''
-    This module returns the location of a user
-    with a github api-url.
-'''
+""" This module returns the location of a user
+    with a GitHub API URL."""
+
 import requests
 import sys
-from datetime import datetime, timedelta
 from datetime import datetime, timedelta
 
 if __name__ == '__main__':
@@ -16,18 +14,22 @@ if __name__ == '__main__':
     api_url = sys.argv[1]
 
     try:
-        response = requests.get(api_url)
+        response = requests.get(api_url, timeout=5)  # Added timeout to prevent hanging
         if response.status_code == 200:
             user_data = response.json()
             print(user_data.get('location', 'Location not specified'))
         elif response.status_code == 403:
-            reset_time = datetime.fromtimestamp(int(response.headers.get('X-RateLimit-Reset', 0)))
-            now = datetime.now()
-            minutes_remaining = (reset_time - now).total_seconds() // 60
-            print('Reset in {} min'.format(int(minutes_remaining)))
+            reset_timestamp = response.headers.get('X-RateLimit-Reset')
+            if reset_timestamp:
+                reset_time = datetime.fromtimestamp(int(reset_timestamp))
+                now = datetime.now()
+                minutes_remaining = max(0, (reset_time - now).total_seconds() // 60)
+                print(f'Reset in {int(minutes_remaining)} min')
+            else:
+                print('Rate limit exceeded, but reset time unavailable.')
         elif response.status_code == 404:
             print('Not found')
         else:
-            print('Error: {}'.format(response.status_code))
+            print(f'Error: {response.status_code}')
     except requests.exceptions.RequestException as e:
-        print('Request failed: {}'.format(e))
+        print(f'Request failed: {e}')
